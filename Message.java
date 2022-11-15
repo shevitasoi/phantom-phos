72eec3c77f1d8582d4847fd870769dffe508fa9d
Bhanu Mishra <bhanumishra@live.com>
2022-11-15 12:56:17 +0530

    Added first example of OOP.

---
+++ Message.java
@@ -0,0 +1,10 @@
+/**
+ * Prints a short message on Standard output
+ */
+
+public class Message {
+	public void greet() {
+		System.out.println("Hello dear, it's nice to meet you.");
+	}
+}
+
---
+++ Welcome.java
@@ -0,0 +1,11 @@
+/**
+ * Prints a greeting message in OOP way.
+ */
+
+public class Welcome {
+	public static void main(String[] args) {
+		Message msg = new Message();
+		msg.greet();
+	}
+}
+
